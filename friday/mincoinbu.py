5b9205b40bd311b343bdd3ffb012fd292eb27513
Satviky <126392608+Satviky@users.noreply.github.com>
2025-07-18 10:10:58 +0000

    bottum up approach

--- friday/mincoinbu.py
+++ friday/mincoinbu.py
@@ -1,13 +1,15 @@
 import math
 def minCoinsBottomUp(N, coins, T):
     dp = [0 for i in range(N + 1)]
+    # iterate over all states 1..N
     for n in range(1, N + 1):
-
+        # initialize the current answer as maximum
         dp[n] = math.inf
         for i in range(T):
             if (n - coins[i] >= 0):
                 subprob = dp[n - coins[i]]
                 dp[n] = min(dp[n], subprob + 1)
+    # print(dp)
     return dp[n]
 N = 15; coins = [1, 7, 10]
 T = len(coins)
